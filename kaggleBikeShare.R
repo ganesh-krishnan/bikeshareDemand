fdc526cb36ac37758970e4eee515954c7b526336
Ganesh <gkrishnan.gt@gmail.com>
2015-12-20 23:43:28 -0800

    Added rmsle metric

--- kaggleBikeShare.R
+++ kaggleBikeShare.R
@@ -3,9 +3,12 @@ library (Amelia)
 library (plyr)
 library (dplyr)
 library (ggplot2)
+library (lubridate)
+library (doMC)
 
 source ("support.R")
 
+registerDoMC (cores=2)
 train.df <- read.csv ("data/train.csv")
 test.df <- read.csv ("data/test.csv")
 
@@ -13,11 +16,26 @@ train.df <- formatData (train.df) %>% tbl_df()
 test.df <- formatData (test.df) %>% tbl_df()
 
 ctrl <- trainControl(method ="repeatedcv", 
-                     number = 10,
-                     repeats = 3)
+                     number = 5,
+                     repeats = 1,
+                     summaryFunction = computeRMSLE)
 
-rf.fit <- train (count ~ . - casual - registered,
+rf.fit <- train (count ~ . - casual - registered - datetime,
+                 data = train.df,
                  method = "rf",
                  preProcess = c("center", "scale"),
-                 trControl = ctrl
+                 trControl = ctrl,
+                 metric = "rmsle",
+                 maximize = FALSE
                  )
+
+test.df$casual=1
+test.df$registered=1
+y.pred <- predict (rf.fit, test.df)
+result.df <- data.frame (datetime=strftime (test.df$datetime, 
+                                            format="%Y-%m-%d %H:%M:%S", 
+                                            tz="UTC"),
+                         count=as.integer (y.pred)
+                         )
+
+write.csv (result.df, "result.csv", quote=FALSE, row.names=FALSE)
--- support.R
+++ support.R
@@ -1,5 +1,19 @@
 formatData <- function (df)
 {
-        df$datetime <- as.POSIXct (df$datetime)
+        df$datetime <- ymd_hms (df$datetime)
+        df$month <- month (df$datetime)
+        df$day <- day (df$datetime)
         df
+}
+
+computeRMSLE <- function (data, lev=NULL, model=NULL)
+{
+        if (!is.null (lev) & !is.na (lev))
+                stop (paste ("RMSLE metric is only applicable to regression"))
+        
+        rmsle <- sqrt (mean ((log (1 + data$obs) - log (1 + data$pred))^2))
+        names (rmsle) <- "rmsle"
+        
+        print (rmsle)
+        rmsle
 }
