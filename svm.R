80b0b23db84e8f7718a9a87d2eebc1fe690e6eac
Ganesh <gkrishnan.gt@gmail.com>
2015-12-30 22:12:24 -0800

    renamed kaggleBikeShare.R as rf.R added svm.R to implement svm on dataset

---
+++ svm.R
@@ -0,0 +1,43 @@
+library (caret)
+library (Amelia)
+library (plyr)
+library (dplyr)
+library (ggplot2)
+library (lubridate)
+library (doMC)
+
+source ("support.R")
+
+registerDoMC (cores=2)
+train.df <- read.csv ("data/train.csv")
+test.df <- read.csv ("data/test.csv")
+
+train.df <- formatData (train.df) %>% tbl_df()
+test.df <- formatData (test.df) %>% tbl_df()
+
+ctrl <- trainControl(method ="repeatedcv", 
+                     number = 5,
+                     repeats = 1,
+                     summaryFunction = computeRMSLE)
+
+svm.fit <- train (count ~ season + holiday + workingday + weather + temp + atemp +
+                         humidity + windspeed + year + month + wday + day + hour,
+                 data = train.df,
+                 method = "svmRadial",
+                 preProcess = c("center", "scale"),
+                 trControl = ctrl,
+                 tuneLength = 3,
+                 metric = "rmsle",
+                 maximize = FALSE
+)
+
+test.df$casual=1
+test.df$registered=1
+y.pred <- predict (svm.fit, test.df)
+result.df <- data.frame (datetime=strftime (test.df$datetime, 
+                                            format="%Y-%m-%d %H:%M:%S", 
+                                            tz="UTC"),
+                         count=as.integer (y.pred)
+)
+
+write.csv (result.df, "result-svm.csv", quote=FALSE, row.names=FALSE)
