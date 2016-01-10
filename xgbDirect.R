29a2ebe7ed5e8b1253b38cd0aa228cca0dc2c818
Ganesh <gkrishnan.gt@gmail.com>
2016-01-10 10:36:52 -0800

    Polished up code to generate predictions

--- xgbDirect.R
+++ xgbDirect.R
@@ -12,10 +12,11 @@ test.df <- read.csv ("data/test.csv")
 train.df <- formatData (train.df, logTransform = TRUE) %>% tbl_df()
 test.df <- formatData (test.df) %>% tbl_df()
 
-train.formula <- count ~ season + holiday + workingday + weather + temp + atemp +
+train.formula <-  ~ season + holiday + workingday + weather + temp + atemp +
         humidity + windspeed + year + month + wday + day + hour - 1
 
 trainData <- xgb.DMatrix (model.matrix (train.formula, train.df), label=train.df$count)
+testData <- xgb.DMatrix (model.matrix (train.formula, test.df))
 
 params <- list (booster="gbtree",
                 eta=0.01,
@@ -27,12 +28,12 @@ params <- list (booster="gbtree",
                 objective="reg:linear",
                 eval_metric="rmse")
 
-fit <- xgb.cv (params, trainData, 1000, 5, early.stop.round = 3)
+fit <- xgb.train (params, trainData, nround = 732, nfold = 5)
 
-y.pred <- exp (predict (fit, test.df)) - 1
+y.pred <- exp (predict (fit, testData)) - 1
 result.df <- data.frame (datetime=strftime (test.df$datetime, 
                                             format="%Y-%m-%d %H:%M:%S", 
                                             tz="UTC"),
                          count=as.integer (y.pred))
 
-write.csv (result.df, "result-xgbTree.csv", quote=FALSE, row.names=FALSE)
+write.csv (result.df, "result-xgbDirect.csv", quote=FALSE, row.names=FALSE)
